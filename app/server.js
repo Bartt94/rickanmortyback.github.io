f9eeec14f7969ac37332acc7c96b841a5b0aa618
Bartolo <bartolo.garcia@ferreryasociados.mx>
2023-04-28 03:44:12 -0600

    Segunda versión FRONT

--- app/server.js
+++ app/server.js
@@ -1,24 +1,63 @@
-var express = require('express') //llamamos a Express
-var app = express()               
+var express = require('express'); //llamamos a Express
+var app = express();
 
-var port = process.env.PORT || 8080  // establecemos nuestro puerto
+// const express = require("express");
+const request = require("request");
+const cors = require("cors");
+// const exphbs = require("express-handlebars");
+// const app = express();
+
+// app.engine("handlebars",exphbs());
+// app.set("view engine","handlebars");
+
+app.use(
+cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders: "*",
+}));
+
+var port = process.env.PORT || 8080 ; // establecemos nuestro puerto
 
 app.get('/', function(req, res) {
   res.json({ mensaje: '¡Hola Mundo!' })   
-})
+});
 
-app.get('/cervezas', function(req, res) {
-  res.json({ mensaje: '¡A beber cerveza!' })  
-})
+app.get("/personajes",(req,res)=>{
+    request("https://rickandmortyapi.com/api/character/",(error,response,body)=>{
+
+      if (error) {
+          return res.status(400).json({
+            status: 'error',
+            error: 'req body cannot be empty',
+          });
+      }else{
+          const personajesReick = JSON.parse(body);
+
+          res.status(200).json({
+              headers: {
+                  "Access-Control-Allow-Headers" : "Content-Type",
+                  "Access-Control-Allow-Origin": "https://rickandmortyapi.com",
+                  "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+              },
+              status: 'succes',
+              data: personajesReick.results,
+          });
+      }
+
+    })
+});
+
 
 app.post('/', function(req, res) {
   res.json({ mensaje: 'Método post' })   
-})
+});
 
 app.del('/', function(req, res) {
   res.json({ mensaje: 'Método delete' })  
-})
+});
 
 // iniciamos nuestro servidor
-app.listen(port)
-console.log('API escuchando en el puerto ' + port)
+app.listen(port);
+console.log('API escuchando en el puerto ' + port);
+
